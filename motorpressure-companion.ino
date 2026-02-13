813945c09cb38c25fdc616fcd34e4f4bcdd1927e
Josh Carlson <22630098+paperman5@users.noreply.github.com>
2026-02-12 18:55:50 -0800

    Trigger ADC with and RTC interrupt

--- motorpressure-companion.ino
+++ motorpressure-companion.ino
@@ -3,6 +3,7 @@
 #define CHANNELS 10
 #define BOARD_ID 0x7
 #define UPDATE_RATE 100 // 100Hz ticks
+#define SAMPLE_RATE 10 // ADC sample rate in milliseconds
 
 // WIRING (Adafruit Feather M0 Adalogger)
 // Flight Computer MOSI <-> Feather PA12 (Labelled MISO)
@@ -58,16 +59,15 @@ typedef struct {
 Sercom4SPISlave SPISlave;
 AltOSMessageHeader message;
 AltOSSetupReply setupReply;
-AltOSFetchReply fetchReply;
+volatile AltOSFetchReply fetchReply;
 uint8_t rxIndex = 0;
 uint8_t txIndex = 0;
+volatile uint8_t adcIndex = 0;
+volatile uint16_t adcSamples[CHANNELS];
 volatile bool newMessage = false;
-volatile uint16_t adcValue = 0;
 
 
-void handleStateChange(State oldState, State newState) {
-  Serial.printf("State Change: %d -> %d\n", (int)oldState, (int)newState);
-}
+/****** MAIN PROGRAM ******/
 
 void setup() {
   Serial.begin(115200);
@@ -82,13 +82,18 @@ void setup() {
   for (int i = 0; i < CHANNELS; i++) {
     fetchReply.data[i] = i;
   }
+  // memset(&adcSamples, 0, sizeof(adcSamples));
   Serial.printf("Setup reply initialized: %d %d %d %d\n", 
                 setupReply.board_id, setupReply.board_id_inverse, 
                 setupReply.update_period, setupReply.channels);
+  init_clocks();
+  Serial.println("System clocks initialized");
   SPISlave.SercomInit(SPISlave.MOSI_Pins::PA12, SPISlave.SCK_Pins::PB09, SPISlave.SS_Pins::PB10, SPISlave.MISO_Pins::PB11);
   Serial.println("SERCOM4 SPI slave initialized");
   init_adc();
   Serial.println("ADC Initialized");
+  init_timer();
+  Serial.println("Timer Initialized");
 }
 
 void loop() {
@@ -98,9 +103,34 @@ void loop() {
     Serial.printf("%d %d %d %d %d %d %d %d %d\n\n", message.command, message.flight_state, message.tick, 
                                                     message.serial, message.flight, message.accel, message.speed, 
                                                     message.height, message.motor_number);
-    Serial.println(adcValue);
     newMessage = false;
   }
+}
+
+/****** PERIPHERAL REGISTER INITIALIZATION ******/
+
+void init_clocks() {
+  // Generic clock 0, used for companion SPI connection (SERCOM4)
+  // Setup with 8Mhz oscillator, no division
+  // NOTE: Not sure why, but touching this crashes the system. Leave it alone.
+  // GCLK->GENDIV.reg = GCLK_GENDIV_ID(0) | GCLK_GENDIV_DIV(0);
+  // while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
+  // GCLK->GENCTRL.reg = GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_OSC8M | GCLK_GENCTRL_ID(0);
+  // while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
+
+  // Generic clock 1, used for ADC clock
+  // Setup with 8Mhz oscillator, no division
+  GCLK->GENDIV.reg = GCLK_GENDIV_ID(1) | GCLK_GENDIV_DIV(0);
+  while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
+  GCLK->GENCTRL.reg = GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_OSC8M | GCLK_GENCTRL_ID(1);
+  while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
+
+  // Generic clock 2, used for Timer clock (RTC)
+  // Setup with 8Mhz oscillator, no division
+  GCLK->GENDIV.reg = GCLK_GENDIV_ID(2) | GCLK_GENDIV_DIV(0);
+  while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
+  GCLK->GENCTRL.reg = GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_OSC8M | GCLK_GENCTRL_ID(2);
+  while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
 }
 
 void init_adc() {
@@ -117,7 +147,7 @@ void init_adc() {
 
   // Configure ADC
   GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(GCM_ADC) | // Enable ADC clock
-                      GCLK_CLKCTRL_ID_ADC |
+                      GCLK_CLKCTRL_GEN_GCLK1 |
                       GCLK_CLKCTRL_CLKEN;
   NVIC_EnableIRQ(ADC_IRQn); // Enable interrupt handler
   NVIC_SetPriority(ADC_IRQn, 1);
@@ -137,13 +167,13 @@ void init_adc() {
   ADC->INTENSET.bit.RESRDY  = 0x1; // Enable result ready interrupt
   ADC->REFCTRL.bit.REFCOMP  = 0x1; // Enable voltage reference compensation
   ADC->REFCTRL.bit.REFSEL   = 0x2; // 3.3V Reference (1/2 VCC = 1.65V)
-  ADC->CTRLB.bit.PRESCALER  = 0x6; // Prescaler 256
+  ADC->CTRLB.bit.PRESCALER  = 0x3; // Prescaler 32
   ADC->CTRLB.bit.RESSEL     = 0x0; // 12-bit resolution mode
                                    // NOTE: Even though there is time for 16 bit oversampling, 
                                    // the signal may change rapidly and might be inaccurate that way.
   ADC->CTRLB.bit.CORREN     = 0x0; // Disable digital correction
-  // ADC->CTRLB.bit.FREERUN    = 0x0; // One-shot conversion mode
-  ADC->CTRLB.bit.FREERUN    = 0x1; // Freerun conversion mode
+  ADC->CTRLB.bit.FREERUN    = 0x0; // One-shot conversion mode
+  // ADC->CTRLB.bit.FREERUN    = 0x1; // Freerun conversion mode
   ADC->CTRLB.bit.DIFFMODE   = 0x0; // Single-ended conversion
   ADC->AVGCTRL.bit.SAMPLENUM = 0x1; // Average 2 samples
   ADC->AVGCTRL.bit.ADJRES   = 0x1; // Divisor of 2 (for 2 samples)
@@ -153,9 +183,55 @@ void init_adc() {
   while (ADC->STATUS.bit.SYNCBUSY);
 }
 
+void init_timer() {
+  // Clock source is GCLK2, setup with 8Mhz oscillator (see init_clocks)
+  GCLK->CLKCTRL.reg = GCLK_CLKCTRL_CLKEN | 
+                      GCLK_CLKCTRL_GEN_GCLK2 | 
+                      GCLK_CLKCTRL_ID(GCM_RTC);
+  // Reset RTC
+  while (GCLK->STATUS.bit.SYNCBUSY);
+  RTC->MODE0.CTRL.bit.ENABLE = 0x0;
+  while (RTC->MODE0.STATUS.bit.SYNCBUSY);
+  RTC->MODE0.CTRL.bit.SWRST = 0x1;
+  while (RTC->MODE0.STATUS.bit.SYNCBUSY);
+
+  NVIC_EnableIRQ(RTC_IRQn); // Enable interrupts
+  NVIC_SetPriority(RTC_IRQn, 0);
+
+  RTC->MODE0.CTRL.bit.MODE = 0x0;       // Mode 0 (32 bit counter)
+  RTC->MODE0.CTRL.bit.PRESCALER = 0x3;  // DIV8
+  RTC->MODE0.CTRL.bit.MATCHCLR = 0x1;   // Clear timer on compare match (reset to 0)
+  RTC->MODE0.COMP[0].reg = (uint32_t) 1000 * SAMPLE_RATE - 1;
+  // RTC->MODE0.INTENSET.bit.CMP0 = 0x1;   // Compare match interrupt enabled
+                                        // Interrupt is raised on the next clock cycle
+  // RTC->MODE0.FREQCORR.bit.SIGN = 0x1;
+  // RTC->MODE0.FREQCORR.bit.VALUE = 0x7F;
+  RTC->MODE0.CTRL.bit.ENABLE = 0x1;
+  while (RTC->MODE0.STATUS.bit.SYNCBUSY);
+}
+
+/****** INTERRUPT HANDLERS ******/
+
+void RTC_Handler() {
+  RTC->MODE0.INTFLAG.bit.CMP0 = 0x1; // Clear the compare match interrupt
+  // TODO: Make sure the RTC is actually ticking at the correct rate.
+  // w/ 8MHz clock and 1s sample rate, it appears to tick at 1003ms. (x1.00259690)
+  // Not sure if that is an issue with the timer setup or something else.
+
+  // Trigger an ADC conversion.
+  if (adcIndex < CHANNELS) {
+    ADC->SWTRIG.bit.START = 0x1;
+  }
+  else {
+    adcIndex = 0;
+    disable_rtc_interrupts();
+  }
+}
+
 void ADC_Handler() {
-  // With prescaler 256, freerun mode, 2 sample average, conversion takes ~110us
-  adcValue = ADC->RESULT.reg; // This will clear the INTFLAG.RESRDY interrupt flag
+  // With prescaler 32, freerun mode, 2 sample average, conversion takes ~54us
+  uint16_t adcValue = ADC->RESULT.reg; // This will clear the INTFLAG.RESRDY interrupt flag
+  fetchReply.data[adcIndex++] = adcValue;
 }
 
 void SERCOM4_Handler()
@@ -196,6 +272,11 @@ Reference: Atmel-42181G-SAM-D21_Datasheet section 26.8.6 on page 503
     txIndex = 0;
     newMessage = true;
     SERCOM4->SPI.INTFLAG.bit.TXC = 1; // Clear Transmit Complete interrupt
+
+    // Start ADC sampling for the next fetch request
+    if (message.command == 2 && !rtc_interrupts_enabled()) {
+      start_rtc();
+    }
   }
   
   // Data Register Empty interrupt: Ready to transmit a single byte
@@ -226,6 +307,8 @@ Reference: Atmel-42181G-SAM-D21_Datasheet section 26.8.6 on page 503
   }
 }
 
+/****** UTILITY ******/
+
 // Prints the raw bytes of an object.
 // Note that the Cortex M0 uses little-endian byte order.
 void print_raw(void* obj, size_t size) {
@@ -234,4 +317,22 @@ void print_raw(void* obj, size_t size) {
     Serial.printf("%02X%s", *(buf + i), (i + 1) % 8 == 0 ? "\n" : " ");
   }
   Serial.println();
+}
+
+void enable_rtc_interrupts() {
+  RTC->MODE0.INTENSET.bit.CMP0 = 0x1;
+}
+
+void disable_rtc_interrupts() {
+  RTC->MODE0.INTENCLR.bit.CMP0 = 0x1;
+}
+
+bool rtc_interrupts_enabled() {
+  return RTC->MODE0.INTENSET.bit.CMP0;
+}
+
+void start_rtc() {
+  RTC->MODE0.COUNT.reg = 0x0;
+  enable_rtc_interrupts();
+  while (RTC->MODE0.STATUS.bit.SYNCBUSY); // COUNT register needs sync
 }
